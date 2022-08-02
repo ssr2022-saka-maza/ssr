449944c3afa6d1a4724a0e0a2fa16479002e5e48
H1rono <hronok66@gmail.com>
2022-08-03 03:39:11 +0900

    PS4の例を追加 #4

---
+++ examples/ps4_log/src/main.cpp
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <ssr/PS4.hpp>
+
+ssr::PS4<ssr::PS4Connection::bluetooth> ps4(true); // Bluetooth接続の場合 初期ペアリングの時trueを渡す
+//ssr::PS4<ssr::PS4Connection::usb> ps4; // USB接続の場合 Bluetoothの行(上の1行)をコメントアウトすること
+
+void setup() {
+    Serial.begin(115200);
+    if (ps4.begin() == -1) {
+        Serial.println("failed to initialize");
+        while (1); // Halt
+    }
+    Serial.println("start");
+}
+
+void loop() {
+    ps4.update();
+    if (!ps4.connected()) {
+        Serial.println("not connected");
+        return;
+    }
+    ssr::PS4Value value;
+    ps4.read(&value);
+    char log[256] = "";
+    sprintf(
+        log,
+        "(%3d %3d) (%3d %3d) [%d %d %d %d] [%d %d %d %d] [%d %d %d] [%d %3d %d] [%d %3d %d]\n",
+        value.lstick.x, value.lstick.y,
+        value.rstick.x, value.rstick.y,
+        value.up,       value.right,  value.down,  value.left,
+        value.triangle, value.circle, value.cross, value.square,
+        value.ps, value.share, value.options,
+        value.l1, value.l2,    value.l3,
+        value.r1, value.r2,    value.r3
+    );
+    Serial.print(log);
+}
